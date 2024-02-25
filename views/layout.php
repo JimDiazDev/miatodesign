5ebbdd52fc2f340744cc84b450d46d2ff5727bc3
JimDev <jamindiazdev@gmail.com>
2024-02-25 16:33:36 -0600

    version 2.7

--- views/layout.php
+++ views/layout.php
@@ -71,18 +71,6 @@
 
     <footer>
         <h3>Miato Design</h3>
-        <div class="footer__links">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
-                <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                <path d="M16.5 7.5l0 .01" />
-              </svg>
-        </div>
         <div class="footer__line"></div>
         <p class="footer__end"><?php echo $depends ? 'Todos los derechos reservados' : 'All rights reserved'; ?> MiatoDesign <?php echo date('Y'); ?>  &copy;</p>
     </footer>
